459d25dde4d4530db61a774b7483390865dc9d7e
Michał <112748466+Miteals@users.noreply.github.com>
2023-02-01 21:21:28 +0100

    Update old_files_remover.sh

--- Maintenance_scripts/old_files_remover.sh
+++ Maintenance_scripts/old_files_remover.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 #Script finds and deletes all files older than x days in directory
 
-search_dir=@1
-file_age=@2
+search_dir=$1
+file_age=$1
 
 find $search_dir -type f -mtime +$file_age -delete
